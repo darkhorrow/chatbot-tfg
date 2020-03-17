378e86456e3a094a0db26dac5bbdb7d44e14df68
Your Name <benearosemidan@hotmail.com>
2020-03-17 13:24:02 +0000

    Stories expansion. Doest work yet

--- actions.py
+++ actions.py
@@ -11,13 +11,54 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
 
 
-class ActionFeedback1(Action):
+class ActionFeedbackLow(Action):
 
     def name(self) -> Text:
-        return "action_give_feedback_1"
+        return "action_give_feedback_low"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(text=tracker.get_slot(key='answer_1'))
-        return []
+        dispatcher.utter_message(text="LOW")
+        return [SlotSet("answer", "low")]
+
+
+class ActionFeedbackLowMedium(Action):
+
+    def name(self) -> Text:
+        return "action_give_feedback_low_medium"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text="LOW-MEDIUM")
+        return [SlotSet("answer", "low-medium")]
+
+
+class ActionFeedbackMedium(Action):
+
+    def name(self) -> Text:
+        return "action_give_feedback_medium"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text="MEDIUM")
+        return [SlotSet("answer", "medium")]
+
+
+class ActionFeedbackMediumHigh(Action):
+
+    def name(self) -> Text:
+        return "action_give_feedback_medium_high"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text="MEDIUM-HIGH")
+        return [SlotSet("answer", "medium-high")]
+
+
+class ActionFeedbackHigh(Action):
+
+    def name(self) -> Text:
+        return "action_give_feedback_high"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text="HIGH")
+        return [SlotSet("answer", "high")]
--- data/stories.md
+++ data/stories.md
@@ -1,7 +1,34 @@
-## ask question 1
+## ask question 1 low intent
 * greet
     - utter_ask_question_1
-* answer{"answer_1": "en ocasiones"}
-    - slot{"answer_1": "en ocasiones"}
-    - action_give_feedback_1
+* answer{"answer": "low"}
+    - slot{"answer": "low"}
+    - action_give_feedback_low
+
+## ask question 1 low-medium intent
+* greet
+    - utter_ask_question_1
+* answer{"answer": "low-medium"}
+    - slot{"answer": "low-medium"}
+    - action_give_feedback_low_medium
+
+## ask question 1 medium intent
+* greet
+    - utter_ask_question_1
+* answer{"answer": "medium"}
+    - slot{"answer": "medium"}
+    - action_give_feedback_medium
+
+## ask question 1 medium-high intent
+* greet
+    - utter_ask_question_1
+* answer{"answer": "medium-high"}
+    - slot{"answer": "medium-high"}
+    - action_give_feedback_medium_high
     
+## ask question 1 high intent
+* greet
+    - utter_ask_question_1
+* answer{"answer": "high"}
+    - slot{"answer": "high"}
+    - action_give_feedback_high
